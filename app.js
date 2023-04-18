0414816de9a9b8c02525bf33556affc14cef22f8
lincoln wasden <lincolnwasden@ip-192-168-4-55.ec2.internal>
2023-04-18 15:29:06 -0600

    here

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const devices = ["phone", "tablet", "computer"];
 
@@ -10,25 +10,41 @@ const randomDeviceType = () =>
   devices[Math.floor(Math.random() * devices.length)];
 const randomGender = () => (Math.random() < 0.5 ? "male" : "female");
 
+const headers = [
+  "Date",
+  "CTR",
+  "CPC",
+  "ROAS",
+  "Spend",
+  "AOV",
+  "Bounce Rate",
+  "Time on Page",
+  "Device Type",
+  "Revenue",
+  "Orders",
+  "Gender",
+];
+
 let data = [];
 
 const generateData = () => {
   data = [];
+  data.push(headers); // Add headers to the data array
   for (let i = 0; i < 100000; i++) {
-    data.push({
-      date: new Date().toISOString().slice(0, 10),
-      ctr: Math.random(),
-      cpc: Math.random() * 10,
-      roas: Math.random() * 5,
-      spend: Math.random() * 1000,
-      AOV: Math.random() * 200,
-      bounceRate: randomBounceRate(),
-      timeOnPage: randomTimeOnPage(),
-      deviceType: randomDeviceType(),
-      revenue: Math.random() * 5000,
-      orders: Math.floor(Math.random() * 100),
-      gender: randomGender(),
-    });
+    data.push([
+      new Date().toISOString().slice(0, 10),
+      Math.random(),
+      Math.random() * 10,
+      Math.random() * 5,
+      Math.random() * 1000,
+      Math.random() * 200,
+      randomBounceRate(),
+      randomTimeOnPage(),
+      randomDeviceType(),
+      Math.random() * 5000,
+      Math.floor(Math.random() * 100),
+      randomGender(),
+    ]);
   }
 };
 
@@ -39,10 +55,6 @@ app.get("/api/data", (req, res) => {
   res.json(data);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:\${port}`);
+  console.log(`API listening at http://localhost:\${port}`);
 });
